78d799734b2d216d383d76b98f335ddf40f0074c
panagiotisfilipp <filipp1977@gmail.com>
2018-11-01 11:03:03 +0200

    επιλογη για αναζητηση με βαση τον τυπο.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 		var errstr  = '';  
 		
 		
-		<!-- search_string: Ελέγχει αν το πεδίο είναι null  -->
-		if ( cForm.search_string.value==''){
+		<!-- search_author: Ελέγχει αν το πεδίο είναι null  -->
+		if ( cForm.search_author.value==''){
 			errstr = errstr+'Το πεδίο συγγραφέας δεν μπορεί να είναι κενό.\n';
-			cForm.search_string.style.backgroundColor="#ff6347";
+			cForm.search_author.style.backgroundColor="#ff6347";
 		}
 	
 		
@@ -39,26 +39,40 @@
 	<tr>
             <th align="center" colspan='1'>ΣΥΓΓΡΑΦΕΑΣ(*):</th>
 			<td align='left'>
-			<?php echo"<select name='search_string';>
-			<option>Adamidis</option>
-			<option>Diamantaras</option>
-			<option>Chatzimisios</option>
-			<option>Ilioudis</option>
-			<option>salampasis</option>
-			<option value='ougiaroglou'>ougiaroglou stefanos</option>
-			<option>Karamitopoulos</option>
-			<option>Dervos</option>
-			<option>Papanikolaou</option>
-			<option>Sidiropoulos</option>
-			<option>Vozalis</option>
-			<option>Goulianas</option>
-			<option>Keramopoulos</option>
+			<?php echo"<select name='search_author';>
+			<option value='adamidis'>ΑΔΑΜΙΔΗΣ ΠΑΝΑΓΙΩΤΗΣ</option>
+			<option value='diamantaras'>ΔΙΑΜΑΝΤΑΡΑΣ ΚΩΣΤΑΝΤΙΝΟΣ</option>
+			<option value='chatzimisios'>ΧΑΤΖΗΜΙΣΙΟΣ ΠΕΡΙΚΛΗΣ</option>
+			<option value='ilioudis'>ΙΛΙΟΥΔΗΣ ΧΡΗΣΤΟΣ</option>
+			<option value='salampasis'>ΣΑΛΑΜΠΑΣΗΣ ΜΙΧΑΛΗΣ</option>
+			<option value='ougiaroglou'>ΟΥΓΙΑΡΟΓΛΟΥ ΣΤΕΦΑΝΟΣ</option>
+			<option value='karamitopoulos'>ΚΑΡΑΜΙΤΟΠΟΥΛΟΣ ΛΕΩΝΙΔΑΣ</option>
+			<option value='dervos'>ΔΕΡΒΟΣ ΔΗΜΗΤΡΙΟΣ</option>
+			<option value='papanikolaou'>ΠΑΠΑΝΙΚΟΛΑΟΥ ΑΛΕΞΑΝΔΡΟΣ</option>
+			<option value='sidiropoulos'>ΣΙΔΗΡΟΠΟΥΛΟΣ ΑΝΤΩΝΗΣ</option>
+			<option value='vozalis'>ΒΟΖΑΛΗΣ ΜΑΝΩΛΗΣ</option>
+			<option value='goulianas'>ΓΟΥΛΙΑΝΑΣ ΚΩΣΤΑΝΤΙΝΟΣ</option>
+			<option value='keramopoulos'>ΚΕΡΑΜΟΠΟΥΛΟΣ ΕΥΚΛΕΙΔΗΣ</option>
 			<option></option>
 			</select>";?> 
 			</td>
 			
 		</tr>
+			<tr>
+            <th align="center" colspan='1'>ΤΥΠΟΣ ΔΗΜΟΣΙΕΥΣΗΣ(*):</th>
+			<td align='left'>
+			<?php echo"<select name='search_type';>
+			<option value='Journal%20Articles'>Αρθρα εφημερίδας</option>
+			<option value='Conference%20and%20Workshop%20Papers'>Έγγραφα συνεδρίων και εργαστηρίων</option>
+			<option value='Informal%20Publications'>Άτυπες Δημοσιεύσεις</option>
+			<option value='Editorship'>Σύνταξη</option>
+			<option value='Parts%20in%20Books%20or%20Collections'>Μέρη σε βιβλία ή συλλογές</option>
+			<option value='Books%20and%20Theses'>Βιβλία και Διατριβές</option>
+			<option></option>
+			</select>";?> 
+			</td>
 			
+		</tr>
 		
 			</table>
 			<table align= 'center' border="0" cellspacing="1" style="border-collapse: collapse; color:#000000" bordercolor="#111111" width="10%">
--- json_from_url.php
+++ json_from_url.php
@@ -1,11 +1,12 @@
 <html>
 <meta charset="utf-8"/>
 
-<table border='1'>
+<form action="checkbox_value.php" method="post">
+<table border='1' width="120%">
 	<tbody>
 	<tr>
-			<th colspan="5" align="center">
-			<h3>ΔΗΜΟΣΙΕΥΣΕΙΣ-ΑΡΘΡΑ  ΕΡΕΥΝΗΤΗ</h3><br></th>
+			<th colspan="6" align="center" >
+			<h3>ΔΗΜΟΣΙΕΥΣΕΙΣ-ΑΡΘΡΑ  ΕΡΕΥΝΗΤΗ </h3><br></th>
 		</tr>
 		<tr>
 		    <th>Συγγραφέας</th>
@@ -13,48 +14,68 @@
 			<th>Χρονιά</th>
 			<th>Τύπος</th>
 			<th>URL</th>
+			<th>ΕΠΙΛΟΓΗ</th>
 			
 			
 		</tr>
-<?php 
-		$search_string=$_POST['search_string'];
-		echo $search_string;
-		$url = "http://dblp.org/search/publ/api?q=$search_string&format=json"; // path to your JSON file
+<?php
+       //http://dblp.org/search/publ/api?q=ougiaroglou+Journal+Articles&format=json
+        include ('conf.php');//ρυθμίσεις βάσης 
+		$search_author=$_POST['search_author'];//metabliti author apo post
+		$search_type=$_POST['search_type'];//metabliti type apo post
+	    $type_out_ch=array();
+		$type_out_ch[0] = urldecode($search_type);//metabliti tipou xoris '%20'
+		//echo "metabliti xoris kena:".$type_out_ch[0].'<br>';//elegox metablitis 
+		//echo $search_author.'<br>';//elegxos metablitis author
+		//echo $search_type.'<br>';//elegxos typou dimosieusis
+		
+		$url = "http://dblp.org/search/publ/api?q=$search_author+$search_type&format=json"; // path to your JSON file
+		//echo $url.'<br>';//elegxos url
         $data = file_get_contents($url); // put the contents of the file into a variable
-
         $characters = json_decode($data,true); // decode the JSON feed
         $result = array();
-		if(!empty($result)){
-			
+	    
+		//elegxos iparjis tetoiou typou dimosieusi me basi ta arxeia pou vrethikan
+		 echo "<b>Οι Δημοσιευσεις του συγγραφέα που επιλέξατε είναι :</b>". $characters['result']['hits']['@sent'];//minima pou emfanizei to sinolo ton dimosieuseon
+		if(($characters['result']['hits']['@sent']==0))//elegxo tin timi tou value tou json
+		{
+			echo '<script language="javascript">alert("Ο συγκεκριμένος συγγραφέας δεν έχει δημοσιεύσει κείμενο με αυτόν τον τύπο !\nΠροσπαθήστε ξανά!");document.location="index_st.php";</script>'; 
+			break;
+		}
+	
+		else
 		 foreach ($characters['result']['hits']['hit'] as $theentity) :
 		 
-		 $length_author = count($theentity['info']['authors']['author']);
-	    
-	
-		
-       
-
+		 $length_author = count($theentity['info']['authors']['author']);//mikos pinaka author
 ?>
+        
         <tr>
+			
 		    <td> <?php 
-	           for($i=0;$i<$length_author;$i++){
+	          for($i=0;$i<$length_author;$i++){
 	           echo $theentity['info']['authors']['author'][$i].',';
 	                                           } ?> </td>
             <td> <?php  echo $theentity['info']['title'];?> </td>
 			<td> <?php echo $theentity['info']['year']; ?> </td>
 			<td> <?php echo $theentity['info']['type']; ?> </td>
+		
 			<td><a href="<?php echo  $theentity['info']['url']; ?>"><img src="/images/view.png"></a> </td>
+			<td> <?php echo "<input type='checkbox' name='check_list[]' value='".$theentity['url']."'<br/>";?> </td>
 			
-		 
+           		 
         </tr>
-<?php endforeach; 
-		}
-	else
-	{echo "the author does not exist";}
-?>
-	
+		
+			
+			<?php echo"<input type='hidden' name='insupd' value='insert'>"; ?>  
+<?php endforeach; ?>
+
 	</tbody>
+		<tr>
+			<td  align='center' colspan='6'><br>
+				<input style="color:red" type='submit' value='ΕΙΣΑΓΩΓΗ ΣΤΗ ΒΑΣΗ' name='submit'/></td>
+		</tr>
 </table>
+</form>
 </html>
 
 
