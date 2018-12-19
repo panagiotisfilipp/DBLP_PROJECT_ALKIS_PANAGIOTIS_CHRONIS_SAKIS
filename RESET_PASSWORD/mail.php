3aed8ea5962972acf85ceb751fa1365588fd351c
ΠΑΝΑΓΙΩΤΗΣ ΦΙΛΙΠΠΑΚΗΣ <33731989+panagiotisfilipp@users.noreply.github.com>
2018-12-19 11:16:29 +0200

    Update mail.php

--- RESET_PASSWORD/mail.php
+++ RESET_PASSWORD/mail.php
@@ -6,17 +6,17 @@ $mail->IsSMTP();
 // enable SMTP authentication
 $mail->SMTPAuth = true;                  
 // GMAIL username
-$mail->Username = "it.350pkb@gmail.com";
+$mail->Username = "adminpkb@gmail.com";
 // GMAIL password
-$mail->Password = "@er0p0ri@350!";
+$mail->Password = "pass";
 $mail->SMTPSecure = "ssl";  
 // sets GMAIL as the SMTP server
 $mail->Host = "smtp.gmail.com";
 // set the SMTP port for the GMAIL server
 $mail->Port = "465";
-$mail->From='it.350pkb@gmail.com';
+$mail->From='adminpkb@gmail.com';
 $mail->FromName='Admin';
-$mail->AddAddress('it.350pkb@gmail.com', 'Admin');
+$mail->AddAddress('adminpkb@gmail.com', 'Admin');
 $mail->Subject  =  'SMTP Mail Testing';
 $mail->IsHTML(true);
 $mail->Body    = 'Hi there , This is just a testing mail';
