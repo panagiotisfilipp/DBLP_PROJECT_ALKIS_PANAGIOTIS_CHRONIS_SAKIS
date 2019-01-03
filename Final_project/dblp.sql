a261e1271889bb5344fcfdab1cce25c059a62c36
ΠΑΝΑΓΙΩΤΗΣ ΦΙΛΙΠΠΑΚΗΣ <33731989+panagiotisfilipp@users.noreply.github.com>
2019-01-03 16:34:16 +0200

    allagi basis gia name and surname users.

--- Final_project/dblp.sql
+++ Final_project/dblp.sql
@@ -83,7 +83,9 @@ CREATE TABLE `users` (
   `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `role` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+  `role` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '1',
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
