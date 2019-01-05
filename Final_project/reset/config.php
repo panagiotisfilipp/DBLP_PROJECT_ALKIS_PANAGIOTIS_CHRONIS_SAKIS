057783407a5d01ea5c023cb738703203db0b2f78
ΠΑΝΑΓΙΩΤΗΣ ΦΙΛΙΠΠΑΚΗΣ <33731989+panagiotisfilipp@users.noreply.github.com>
2019-01-05 10:18:00 +0200

    Update config.php

--- Final_project/reset/config.php
+++ Final_project/reset/config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "alkibiad_dblp";
-$password = "dblp2018";
-$dbname = "alkibiad_dblp";
+$username = "xxxxxxx";
+$password = "xxxxxx";
+$dbname = "xxxxxx";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
