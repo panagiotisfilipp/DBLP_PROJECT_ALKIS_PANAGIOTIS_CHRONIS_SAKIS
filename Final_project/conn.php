4fbaef3154faed6bf2c2047df628e33566ebd441
ΠΑΝΑΓΙΩΤΗΣ ΦΙΛΙΠΠΑΚΗΣ <33731989+panagiotisfilipp@users.noreply.github.com>
2019-01-06 16:14:22 +0200

    diagrafi arxeion pou den xreiazonati.

--- Final_project/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-//MySQLi Procedural
-$conn = mysqli_connect('localhost','xxxxxx','xxxxxxx','xxxxxxx');
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-mysqli_set_charset($conn,"utf8");
-?>
